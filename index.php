434e137d024617d9e336a4f5b882e706cd5ff9e6
sort1554 <asorto@csumb.edu>
2015-02-11 19:29:06 +0000

    added numbers to the <table> in index.php for dev purposes

--- index.php
+++ index.php
@@ -20,56 +20,57 @@
     <div id="wrapper">
         <h1>SilverJack</h1>
         
-        <?php
-            include 'includes/functions.php';
-        ?>    
+        
+           
         
         <div id="game">
-            
-            <table>
+            <?php
+            include 'includes/functions.php';
+            echo "<table>
                 
                 <!-- row 1 -->
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td colspan="4"></td>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                    
                 </tr>
                 <!-- row 2 -->
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>9</td>
+                    <td>10</td>
+                    <td>11</td>
+                    <td>12</td>
+                    <td>13</td>
+                    <td>14</td>
+                    <td>15</td>
                     
                 </tr>
                 <!-- row 3 -->
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>16</td>
+                    <td>17</td>
+                    <td>18</td>
+                    <td>19</td>
+                    <td>20</td>
+                    <td>21</td>
+                    <td>22</td>
                     
                 </tr>
                 <!-- row 4 -->
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>23</td>
+                    <td>24</td>
+                    <td>25</td>
+                    <td>26</td>
+                    <td>27</td>
+                    <td>28</td>
+                    <td>29</td>
+                    <td colspan='4'>8</td>
                     
                 </tr>
                 
@@ -77,7 +78,8 @@
             
             <button >
                 Play Again
-            </button>
+            </button>"
+            ?> 
         </div>
         <div id="rules">
             <h3>Rules:</h3>
