854256b2cb6bf6f1c758044ee4b4d8952d3ead23
sort1554 <asorto@csumb.edu>
2015-02-11 19:06:13 +0000

    created basic elements to index.php file

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <!--
-To change this template use Tools | Templates.
+    Document   : labs/cst336_lab2/index.php
+    Created on : 2015-02-11 10:44 AM
+    Author     : sort1554
+    Description: Main page for Silverjack game.
+    
+   
 -->
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>Lab 2: Silverjack</title>
     <meta charset="utf-8" />
+    <link href="css/styles.css" rel="stylesheet">
 </head>
 <body>
+    <div id="wrapper">
+        <h1>SilverJack</h1>
+        <div id="game">
+            
+            
+            <button >
+                Play Again
+            </button>
+        </div>
+        <div id="rules">
+            
+        </div>
+        
+        
+    </div>
 
 </body>
 </html>
